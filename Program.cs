5a7ad52b02ad741fb2418c52c8b79f61d4461e6c
sodizdaroglu <americanlaz@gmail.com>
2020-11-07 23:54:41 +0300

    second initial

--- Program.cs
+++ Program.cs
@@ -10,6 +10,20 @@ namespace Exercisesxxx1
     {
         static void Main(string[] args)
         {
+            /// Write a program and continuously ask the user to enter a number. The loop terminates when the user 
+            /// enters “ok". Calculate the sum of all the previously entered numbers and display it on the console
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
         }
     }
 }
